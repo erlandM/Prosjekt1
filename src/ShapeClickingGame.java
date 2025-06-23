1bb9e9ff66078e21e6847787042240b4d1722ba1
erlandM <erland.96@hotmail.com>
2025-06-23 19:08:11 +0200

    refactored the shapeclass to use java.awt.geom objects

--- src/ShapeClickingGame.java
+++ src/ShapeClickingGame.java
@@ -2,8 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.geom.*;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -109,8 +109,6 @@ public class ShapeClickingGame extends JFrame {
             shapeList.add(randomType);
         }
         
-        // Shuffle the list
-        Collections.shuffle(shapeList);
         
         // Create and position shapes
         for (int i = 0; i < SHAPES_COUNT; i++) {
@@ -155,9 +153,7 @@ public class ShapeClickingGame extends JFrame {
                     
                     startNewRound();
                 } else {
-                    // Wrong shape - visual feedback
-                    shape.color = Color.RED;
-                    gamePanel.repaint();
+                    // Wrong shape clicked
                     round++;
                     if (round >= TOTAL_ROUNDS) {
                         endGame();
@@ -202,69 +198,71 @@ public class ShapeClickingGame extends JFrame {
         ShapeType type;
         Color color;
         final int size = 50;
-        
+        java.awt.Shape awtShape;
+
         public Shape(int x, int y, ShapeType type, Color color) {
             this.x = x;
             this.y = y;
             this.type = type;
             this.color = color;
+            this.awtShape = createAwtShape();
         }
-        
+
+        private java.awt.Shape createAwtShape() {
+            switch (type) {
+                case CIRCLE:
+                    return new Ellipse2D.Double(x - size / 2, y - size / 2, size, size);
+                case SQUARE:
+                    return new Rectangle2D.Double(x - size / 2, y - size / 2, size, size);
+                case TRIANGLE: {
+                    Polygon triangle = new Polygon();
+                    triangle.addPoint(x, y - size / 2);
+                    triangle.addPoint(x - size / 2, y + size / 2);
+                    triangle.addPoint(x + size / 2, y + size / 2);
+                    return triangle;
+                }
+                case DIAMOND: {
+                    Polygon diamond = new Polygon();
+                    diamond.addPoint(x, y - size / 2);
+                    diamond.addPoint(x - size / 2, y);
+                    diamond.addPoint(x, y + size / 2);
+                    diamond.addPoint(x + size / 2, y);
+                    return diamond;
+                }
+                case STAR:
+                    return createStarShape(x, y, size / 2, size / 4, 5);
+                default:
+                    return null;
+            }
+        }
+
+        private java.awt.Shape createStarShape(int centerX, int centerY, int outerRadius, int innerRadius, int numPoints) {
+            Path2D star = new Path2D.Double();
+            double angleStep = Math.PI / numPoints;
+            for (int i = 0; i < numPoints * 2; i++) {
+                double angle = i * angleStep - Math.PI / 2;
+                int radius = (i % 2 == 0) ? outerRadius : innerRadius;
+                double xPt = centerX + Math.cos(angle) * radius;
+                double yPt = centerY + Math.sin(angle) * radius;
+                if (i == 0) {
+                    star.moveTo(xPt, yPt);
+                } else {
+                    star.lineTo(xPt, yPt);
+                }
+            }
+            star.closePath();
+            return star;
+        }
+
         public void draw(Graphics2D g2d) {
             g2d.setColor(color);
-            
-            switch (type) {
-                case CIRCLE -> {
-                    g2d.fillOval(x - size/2, y - size/2, size, size);
-                    g2d.setColor(Color.BLACK);
-                    g2d.drawOval(x - size/2, y - size/2, size, size);
-                }
-                    
-                case SQUARE -> {
-                    g2d.fillRect(x - size/2, y - size/2, size, size);
-                    g2d.setColor(Color.BLACK);
-                    g2d.drawRect(x - size/2, y - size/2, size, size);
-                }
-                    
-                case TRIANGLE -> {
-                    int[] xPoints = {x, x - size/2, x + size/2};
-                    int[] yPoints = {y - size/2, y + size/2, y + size/2};
-                    g2d.fillPolygon(xPoints, yPoints, 3);
-                    g2d.setColor(Color.BLACK);
-                    g2d.drawPolygon(xPoints, yPoints, 3);
-                }
-                    
-                case DIAMOND -> {
-                    int[] xDiamond = {x, x - size/2, x, x + size/2};
-                    int[] yDiamond = {y - size/2, y, y + size/2, y};
-                    g2d.fillPolygon(xDiamond, yDiamond, 4);
-                    g2d.setColor(Color.BLACK);
-                    g2d.drawPolygon(xDiamond, yDiamond, 4);
-                }
-                    
-                case STAR -> drawStar(g2d, x, y, size/2, size/4, 5);
-            }
-        }
-        
-        private void drawStar(Graphics2D g2d, int centerX, int centerY, int outerRadius, int innerRadius, int numPoints) {
-            int[] xPoints = new int[numPoints * 2];
-            int[] yPoints = new int[numPoints * 2];
-            
-            for (int i = 0; i < numPoints * 2; i++) {
-                double angle = Math.PI * i / numPoints;
-                int radius = (i % 2 == 0) ? outerRadius : innerRadius;
-                
-                xPoints[i] = centerX + (int) (radius * Math.cos(angle - Math.PI / 2));
-                yPoints[i] = centerY + (int) (radius * Math.sin(angle - Math.PI / 2));
-            }
-            
-            g2d.fillPolygon(xPoints, yPoints, numPoints * 2);
+            g2d.fill(awtShape);
             g2d.setColor(Color.BLACK);
-            g2d.drawPolygon(xPoints, yPoints, numPoints * 2);
+            g2d.draw(awtShape);
         }
-        
+
         public boolean contains(int clickX, int clickY) {
-            return Math.abs(clickX - x) <= size/2 && Math.abs(clickY - y) <= size/2;
+            return awtShape != null && awtShape.contains(clickX, clickY);
         }
     }
     
